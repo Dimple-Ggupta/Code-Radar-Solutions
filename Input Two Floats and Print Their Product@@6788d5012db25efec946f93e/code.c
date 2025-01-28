844cc1558bd1872f28fb95f5cd53153344973d96
Dimple-Ggupta <dimplegupta.02062006@gmail.com>
2025-01-28 16:56:46 +0530

    Solution to Input Two Floats and Print Their Product

--- Input Two Floats and Print Their Product@@6788d5012db25efec946f93e/code.c
+++ Input Two Floats and Print Their Product@@6788d5012db25efec946f93e/code.c
@@ -3,8 +3,8 @@ int main(){
     float a;
     float b;
     float c;
-    scanf("%.2f",&a);
-    scanf("%.2f",&b);
+    scanf("%f",&a);
+    scanf("%f",&b);
     c = a * b;
     printf("Product: %.2f",c);
     return 0;
