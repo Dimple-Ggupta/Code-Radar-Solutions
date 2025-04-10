ca82c33cc2ad29f1f8cd8b9a13405fb1e1444ff6
DIMPLE <dimplegupta.02062006@gmail.com>
2025-04-11 01:05:12 +0530

    Solution to Print a Pyramid Pattern

--- Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
+++ Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-int main(){
+int main() {
     int n;
-    scanf("%d",&n);
-    for ( int i = 1; i <= n ; i++){
-        for ( int j = 1 ; j <=2*i-1 ; j++){
+    printf("Enter pyramid height: ");
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n-i; j++) {
+            printf(" ");
+        }
+        for(int j=1; j<=2*i-1; j++) {
             printf("*");
         }
         printf("\n");
